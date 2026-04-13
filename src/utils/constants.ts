c220356b2088bcec213035898a17b6ea6cadc8fa
Clinton Omondi <kingclinn40931@gmail.com>
2026-04-13 09:36:39 +0300

    chore: update GitHub profile URL in SOCIAL_LINKS constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const SOCIAL_LINKS = {
-  github: "https://github.com/ClintoKe",
+  github: "https://github.com/k-clinton",
   linkedin: "https://linkedin.com/in/kappy-clinton-6ab35824b4",
   twitter: "https://x.com/ClintonKappy",
   email: "clinton.omondi@outlook.com"
